66ffab546f1acc9a581068c9aab4f45f89b27bf1
Eduardo Marques <edu.temarques@gmail.com>
2022-10-10 00:21:11 -0300

    feat: config npm link

--- angular-consumer-app/src/app/app.module.ts
+++ angular-consumer-app/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MastheadModule} from 'angular-external-lib/lib'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, MastheadModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
